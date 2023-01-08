7dd694837d4a77ee6149745b07d1e9037781cee2
bascott5 <bascott1002@gmail.com>
2023-01-08 11:56:37 -0500

    Added in Sass styling.

--- pages/src/pages/home.tsx
+++ pages/src/pages/home.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
     return (
-        <p>Hello World!</p>
+        <p>Websites are a lot like windows: they make people want to see more of what's inside. I carry this philosophy in my web development. Whatever the site's purpose is, I'll make sure it will catch the most eyes possible. 
+        My knowledge of SEO (Search Engine Optimization) and graphic design will provide you with a quality Wordpress website that will be sure to bring in the visitors. Plus, I am versed in both HTML and CSS, including CSS preprocessors like SaaS, allowing me to make your website look all of the more stunning.
+        Check out my work here.</p>
     )
 }
