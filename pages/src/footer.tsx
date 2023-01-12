9bf4cdfa90b03df145efc1e74cd001fec39b41e2
bascott5 <bascott1002@gmail.com>
2023-01-12 16:04:39 -0500

    Modified some of the styling.

--- pages/src/footer.tsx
+++ pages/src/footer.tsx
@@ -1,12 +1,13 @@
 export default function Footer() {
     return (
-        <div className="box">
+        <div>
+            <div className="box"></div>
             <div className="footerLeft">
                 My name is Bennett Scott and my interest in computers has driven me to become a sophomore computer science major at the University of North Carolina at Greensboro. I was originally inspired to get into coding so I could one day build a video game, but have since branched out into web development. I enjoy creating user interfaces that flow into each other well and make it easy for the user to navigate. I also have a bit of an interest in AI, specificially Deep Learning, because of how AI designed in this way can recognize patterns the same way humans can.
                 Beyond my love for coding, I also love Dungeons and Dragons, world building, and video games. Part of the reason I enjoy Dungeons and Dragons is the collaborative element between the players where everyone must work together in order to achieve a desired outcome. I also enjoy interactive narratives and stories with deep lore that contribute to the larger whole of the story that they are meant to serve.
             </div>
             <div className="footerRight">
-                You can contact me at .
+                You can contact me at:
             </div>
         </div>
     )
