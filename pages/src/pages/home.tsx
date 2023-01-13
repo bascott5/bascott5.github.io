034a5c99eaa1a15ab85b90468ec18c9bb0c7733c
bascott5 <bascott1002@gmail.com>
2023-01-12 20:36:39 -0500

    Found ESList bug and fixed it.

--- pages/src/pages/home.tsx
+++ pages/src/pages/home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div>
             <pre className="header">🅑🅔🅝🅝🅔🅣🅣 🅢🅒🅞🅣🅣</pre>
             <div className="text">
-                <p>Websites are a lot like windows: they make people want to see more of what's inside. I carry this philosophy into my web development. Whatever the site's purpose is, whether it's a portfolio or a website for your local business, I'll make sure it will catch the most eyes possible.</p>
+                <p>Websites are a lot like windows: they make people want to see more of what&apos;s inside. I carry this philosophy into my web development. Whatever the site&apos;s purpose is, whether it&apos;s a portfolio or a website for your local business, I&apos;ll make sure it will catch the most eyes possible.</p>
                 <p>My knowledge of SEO (Search Engine Optimization) and graphic design will provide you with a quality, static website that will be sure to bring in the visitors. Plus, I am versed in both HTML and CSS, including CSS preprocessors like Sass, allowing me to make your website look all of the more stunning.</p>
                 <p>Check out my work below:</p>
             </div>
